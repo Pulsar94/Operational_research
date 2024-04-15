5f042d7025054cb64ca5b1184310916ba0d2070a
MarcRG <marc.rougagnou@efrei.net>
2024-04-15 16:04:37 +0200

    pénalités 1er essai

--- Classtab.py
+++ Classtab.py
@@ -19,7 +19,12 @@ class Tab:
             print("No need to add fictif")
         else:
             print("Fictif have been added")
-
+        #penalites
+        choice_pen = self.penalites()
+        if choice_pen[2] == 1:
+            print("ligne", choice_pen[1], "avec une penalité de", choice_pen[0])
+        else:
+            print("colonne", choice_pen[1], "avec une penalité de", choice_pen[0])
 
     def is_command_equal_provider(self):#marc
         if sum(self.command) == sum(self.provider):
@@ -44,9 +49,66 @@ class Tab:
             self.cout.append([0]*len(self.cout[0]))
         return True#fictif have been added
 
+    def copie_tab(self, tab):
+        cop = []
+        for lign in tab:
+            cop.append(lign)
+        return cop
+
     def penalites(self):
-        #penalites
-        pass
+        #choix de si c'est une ligne ou une colonne à prendre et renvoie la valeur de la penalité et sa position
+        #penalites comprenant les fictifs et les non fictifs pour les lignes
+
+        #vérifier qu'il y a pas de soucis avec les fictifs
+        max_row = -1
+        pos_max_row = -1
+
+        for i in range(len(self.cout)):
+            for j in range(len(self.cout[i])):
+                row = self.copie_tab(self.cout[i])
+                row.sort()
+                if row[0] == 0:
+                    diff_row = row[2]-row[1]
+                else:
+                    diff_row = row[1]-row[0]
+                if diff_row > max_row:
+                    max_row = diff_row
+                    pos_max_row = i
+
+        max_col = -1
+        pos_max_col = -1
+        col = self.copie_tab(self.cout)
+        temp_cout = zip(*col)
+        pos_now=0
+        for i in temp_cout:
+            tab_col = list(i)
+            tab_col.sort()
+            #vérifier si l'ensemble des valeurs n'est pas des 0
+            cpt = 0
+            for nb_0 in tab_col:
+                if nb_0 == 0:
+                    cpt += 1
+            if cpt != len(tab_col):#on ne prend pas en compte les rajout de sources fictives
+                if tab_col[0] == 0:
+                    diff_col = tab_col[2]-tab_col[1]
+                else:
+                    diff_col = tab_col[1]-tab_col[0]
+                if diff_col > max_col:
+                    max_col = diff_col
+                    pos_max_col = pos_now
+            pos_now += 1
+
+        final_take = max(max_row, max_col)
+        if final_take == max_row:
+            return final_take, pos_max_row, 1 #1 if it's a row
+        else:
+            return final_take, pos_max_col, 2 #2 if it's a col
+
+
+
+
+
+
 
     def max_penalite(self):#4.2
         #max penalité --> ne pas prenre en compte les pénalités des fictifs
--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from Classtab import Tab
 
 tab = Tab()
 tab.cout= [[6, 3, 8], [5, 7, 4], [8, 2, 3]]
-tab.command = [500, 240, 610]#manque 350 par rapport au provider
-tab.provider = [450, 550, 700]
+tab.command = [500, 240, 610, 350]#manque 350 par rapport au provider
+tab.provider = [450, 550]#manque 700 par rapport au command
+"""
 print(tab.command," command \n")
 print(tab.provider," provider \n")
 print(tab.content," content \n")
 print(tab.cout, "cout \n")
-
+"""
 tab.balas_hammer()
+print("\n")
 print(tab.command, " command \n")
 print(tab.provider, " provider \n")
-print(tab.content, " content \n")
 print(tab.cout, "cout \n")
 

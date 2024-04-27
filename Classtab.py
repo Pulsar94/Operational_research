906c40fbe04119c43e7c8be8100236665a5474e5
MarcRG <marc.rougagnou@efrei.net>
2024-04-27 13:51:44 +0200

    avant le désastre

--- Classtab.py
+++ Classtab.py
@@ -18,21 +18,23 @@ class Tab:
         return False#commande != fournisseur
 
 
-    def add_fictif(self): #marc
+    def add_fictif(self):
         #command/provider fictif
         if self.is_command_equal_provider()==True:
             return False#No need to add fictif
 
-        if sum(self.command) < sum(self.provider):
-            diff = sum(self.provider)-sum(self.command)
-            self.command.append(diff)
-            for i in range(len(self.cout)):
-                self.cout[i].append(0)
-
-        elif sum(self.command) > sum(self.provider):
+        if sum(self.provider) < sum(self.command):
             diff = sum(self.command)-sum(self.provider)
             self.provider.append(diff)
-            self.cout.append([0]*len(self.cout[0]))
+            self.cout.append([0] * len(self.cout[0]))
+
+
+        elif sum(self.provider) > sum(self.command):
+            diff = sum(self.provider)-sum(self.command)
+            self.command.append(diff)
+
+            for i in range(len(self.cout)):
+                self.cout[i].append(0)
         return True#fictif have been added
 
     def copie_tab(self, tab):#obliger de créer une fonction de copie car sinon les valeurs sont liées et modifiées
@@ -41,7 +43,15 @@ class Tab:
             cop.append(lign)
         return cop
 
-    def penalites(self, done_row=[], done_col=[]):#done_row et done_col pos des truc deja remplis
+    def done_numbers_row(self, row, x):#marc
+        #exclu les nombres déja remplis
+        for i in range(len(row)):
+            if self.content[x][i] != -1:
+                row[i] = -1
+
+
+
+    def penalites(self, done_row, done_col):#done_row et done_col pos des truc deja remplis
         #choix de si c'est une ligne ou une colonne à prendre et renvoie la valeur de la penalité et sa position
         #penalites comprenant les fictifs et les non fictifs pour les lignes
 
@@ -50,8 +60,8 @@ class Tab:
         for i in range(len(self.cout)):  # choix de la ligne max de penalité
             if i not in done_row:
                 row = self.copie_tab(self.cout[i])
+                #TODO done numbers à exclure de la ligne
                 row.sort()
-
                 if row[0] == 0:
                     diff_row = row[2] - row[1]
                 else:
@@ -78,6 +88,7 @@ class Tab:
 
         for i in range(len(col_cout_transp)):#même principe que les lignes mais sur le tableau modifié des colonnes
             if i not in done_col:
+                #TODO done numbers à exclure de la colonne
                 col_cout_transp[i].sort()
 
                 if col_cout_transp[i][0] == 0:
@@ -118,14 +129,6 @@ class Tab:
         #choice=1 --> cas ligne
         #choice=3 --> cas combiné
         if choice == 0:
-            cmd_max = self.command[pos]
-            cmd_diff = self.command[pos_diff]
-            if cmd_diff >= cmd_max:
-                return 1
-            else:
-                return 0
-
-        if choice == 1:
             prv_max = self.provider[pos]
             prv_diff = self.provider[pos_diff]
             if prv_diff >= prv_max:
@@ -133,33 +136,67 @@ class Tab:
             else:
                 return 0
 
+        if choice == 1:
+            cmd_max = self.command[pos]
+            cmd_diff = self.command[pos_diff]
+            if cmd_diff >= cmd_max:
+                return 1
+            else:
+                return 0
+
         if choice == 3:
-            final_row = self.command[pos]
-            final_col = self.provider[pos_diff]
+            final_row = self.provider[pos]
+            final_col = self.command[pos_diff]
             if final_row >= final_col:
                 return 0
             else:
                 return 1
 
-    def compare_command_provider_to_fill(self, posx, posy):
-        print("place nb à fill", self.cout[posx][posy])
-        pass
-    def fill_with_penalitie_row(self, pos):
-        temp_min = self.cout[pos][0]
-        for i in range(len(self.cout[pos])):  #TODO cas où il y a 2 couts identiques
-            if self.cout[pos][i] < temp_min:
-                temp_min = self.cout[pos][i]
-                y = i
-            if self.cout[pos][i] == temp_min:
-                pass
-        self.compare_command_provider_to_fill(pos, y)
-        print(temp_min, "temp_min")
+
+    def find_pos(self, x=-1, y=-1):#trouve le plus petit en fonction de la ligne ou de la colonne
+        if y == -1:
+            min = self.cout[x][0]
+            pos = 0
+            for i in range(len(self.cout)):
+                if self.cout[x][i] < min and self.content[x][i] == -1:
+                    min = self.cout[x][i]
+                    pos = i
+            return x, pos
+        else:
+            min = self.cout[0][y]
+            pos = 0
+            for i in range(len(self.cout[0])):
+
+                if self.cout[i][y] < min and self.content[x][i] == -1:
+                    min = self.cout[i][y]
+                    pos = i
+            return pos, y
+
+    def fill_BH(self, x, y, done_row, done_col):
+        if self.provider[x] < self.command[y]:
+            for i in range(len(self.cout[x])):
+                if self.content[x][i] == -1:#Exclu les valeurs déjà remplies
+                    self.content[x][i] = 0
+            self.content[x][y] = self.provider[x]
+            done_row.append(x)
+            self.command[y] -= self.provider[x]
+            return done_row, done_col
+        else:
+            for i in range(len(self.cout)):
+                if self.content[i][y] == -1:
+                    self.content[i][y] = 0
+            self.content[x][y] = self.command[y]
+            done_col.append(y)
+            self.provider[x] -= self.command[y]
+            return done_row, done_col
 
 
-
-    def fill_with_penalitie_col(self, pos):
-        print("pas encore fait")
-        pass
+    def verif_done(self):
+        for i in range(len(self.content)):
+            for j in range(len(self.content[0])):
+                if self.content[i][j] == -1:
+                    return False
+        return True
 
     def balas_hammer(self): #marc
         #savoir si il faut des fictifs
@@ -167,19 +204,46 @@ class Tab:
             print("No need to add fictif")
         else:
             print("Fictif have been added")
+        #TODO faire copie de command et provider pour pouvoir modif tranquille et les remettre à la fin
 
-        #penalites
-        choice_pen = self.penalites()
+        #init -1 self.content
+        self.content = [[-1] * len(self.cout[0]) for _ in range(len(self.cout))]
 
-        #remplissage
-        if choice_pen[2] == 1:
-            print("ligne", choice_pen[1], "avec une penalité de", choice_pen[0])
-            self.fill_with_penalitie_row(choice_pen[1])
+        done_row = []
+        done_col = []
 
-        if choice_pen[2] == 2:
-            print("colonne", choice_pen[1], "avec une penalité de", choice_pen[0])
-            self.fill_with_penalitie_col(choice_pen[1])
-        #faire remplir le plus petit continuer et faire des copies et tout
+        #copie de provider et command
+        provider_before = self.copie_tab(self.provider)
+        command_before = self.copie_tab(self.command)
+
+        print("\n")
+        while(self.verif_done()==False):
+            #penalites
+            choice_pen = self.penalites(done_row, done_col)
+
+            #remplissage
+            if choice_pen[2] == 1:
+                print("ligne", choice_pen[1], "avec une penalité de", choice_pen[0])
+                nb_to_fill = self.find_pos(choice_pen[1])#obtention de x et y du nombre à fill
+                print("cout le plus bas à remplir", self.cout[nb_to_fill[0]][nb_to_fill[1]])
+                self.fill_BH(nb_to_fill[0], nb_to_fill[1], done_row, done_col)
+
+            else:
+                print("colonne", choice_pen[1], "avec une penalité de", choice_pen[0])
+                nb_to_fill = self.find_pos(-1, choice_pen[1])
+                print("cout le plus bas à replir __", self.cout[nb_to_fill[0]][nb_to_fill[1]])
+                self.fill_BH(nb_to_fill[0], nb_to_fill[1], done_row, done_col)
+
+            print("done_row", done_row)
+            print("done_col", done_col)
+            print("cout", self.cout)
+            print("content", self.content)
+            print("command", self.command)
+            print("provider", self.provider)
+            print("\n")
+
+        self.command = command_before
+        self.provider = provider_before
 
 
 
--- main.py
+++ main.py
@@ -6,24 +6,41 @@ from Classtab import Tab
 
 tab = Tab()
 """
-tab.cout = [[6, 3, 8], [5, 7, 4], [8, 2, 3]]
-tab.command = [500, 240, 610]#manque 350 par rapport au provider
-tab.provider = [450, 550, 700]#manque 700 par rapport au command
+tab.cout = [[6, 3, 8],
+            [5, 7, 4],
+            [8, 2, 3]]
+tab.command = [500, 240, 610, 350]#manque 350 par rapport au provider
+tab.provider = [450,
+                550,
+                700]#manque 700 par rapport au command
 """
-"""
-tab.cout = [[15, 9, 8], [2, 5, 2], [3, 1, 6]]
-tab.command = [900, 140, 1000]
-tab.provider = [630, 650, 660]
-"""
-"""
-tab.cout = [[15, 8, 3, ], [14, 8, 3], [13, 4, 3]]
-tab.command = [600, 700, 400]
-tab.provider = [400, 7000, 600]
-"""
-tab.cout = [[15, 8, 3], [8, 6, 2], [3, 4, 1]]
-tab.command = [900, 600, 400]
-tab.provider = [800, 700, 300]
 
+tab.cout = [[15, 9, 8],
+            [2, 5, 2],
+            [3, 1, 6]]
+tab.command = [900, 140, 1000]
+tab.provider = [630,
+                650, 
+                660]
+"""
+
+tab.cout = [[15, 8, 3],
+            [14, 8, 3],
+            [13, 4, 3]]
+tab.command = [600, 700, 400]
+tab.provider = [400,
+                700,
+                600]
+"""
+"""
+tab.cout = [[15, 8, 3], 
+            [8, 6, 2], 
+            [3, 4, 1]]
+tab.command = [700, 600, 400]
+tab.provider = [800, 
+                700, 
+                300]
+"""
 """
 print(tab.command," command \n")
 print(tab.provider," provider \n")
@@ -33,7 +50,10 @@ print(tab.cout, "cout \n")
 
 tab.balas_hammer()
 print("\n")
+"""
 print(tab.command, " command \n")
 print(tab.provider, " provider \n")
 print(tab.cout, "cout \n")
+print(tab.content, " content \n")
 
+"""
